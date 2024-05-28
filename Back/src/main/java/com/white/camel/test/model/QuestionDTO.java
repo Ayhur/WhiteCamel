57480472a71751f87d0e93d73f77021af30835eb
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2024-05-28 19:19:35 +0200

    Quitar setters y getters innecesarios

--- Back/src/main/java/com/white/camel/test/model/QuestionDTO.java
+++ Back/src/main/java/com/white/camel/test/model/QuestionDTO.java
@@ -20,36 +20,4 @@ public class QuestionDTO {
 
 	private CorrectionDTO correction;
 
-	public Long getQuestionId() {
-		return questionId;
-	}
-
-	public void setQuestionId(Long questionId) {
-		this.questionId = questionId;
-	}
-
-	public String getQuestionText() {
-		return questionText;
-	}
-
-	public void setQuestionText(String questionText) {
-		this.questionText = questionText;
-	}
-
-	public List<ResponseDTO> getResponses() {
-		return responses;
-	}
-
-	public void setResponses(List<ResponseDTO> responses) {
-		this.responses = responses;
-	}
-
-	public CorrectionDTO getCorrection() {
-		return correction;
-	}
-
-	public void setCorrection(CorrectionDTO correction) {
-		this.correction = correction;
-	}
-
 }
